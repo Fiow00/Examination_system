281efc80178772ded87ebba24d02a281082d9164
elftiany <mk5920201@gmail.com>
2026-04-12 01:26:24 +0200

    Test Instructor (Exception: Dublicate email)

--- test_procedures/Instructor_test.sql
+++ test_procedures/Instructor_test.sql
@@ -2,5 +2,6 @@ CALL add_instructor('Test Instructor', 'test@iti.com', 1);   --Inserted
 
 CALL add_instructor('', 'test2@iti.com', 1);                --Exception: empty name
 
+CALL add_instructor('Test2', 'test@iti.com', 1);            -- Exteption: Dublicate Name
 
 
