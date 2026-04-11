b3fd80f5f9d90bfee60100c7fac6158cd1c7a716
Fiow00 <ahmedyhya5325500@gmail.com>
2026-04-11 14:37:14 +0200

    Added some optional validaitons for 'course' procedure

--- procedures/courses.sql
+++ procedures/courses.sql
@@ -2,15 +2,30 @@
 CREATE OR REPLACE PROCEDURE "add_course" (name TEXT, min_deg INT, max_deg INT)
 LANGUAGE "plpgsql"
 AS $$
+DECLARE 
+    clean_name TEXT := TRIM(name);
 BEGIN
-    INSERT INTO "course" ("course_name", "min_degree", "max_degree")
-    VALUES (LOWER(TRIM(name)), min_deg, max_deg);
 
-    RAISE NOTICE 'Course "%" added successfully (min=%, max=%)', name, min_deg, max_deg;
+    IF name IS NULL OR clean_name = '' THEN
+        RAISE EXCEPTION 'Course name cannot be empty';
+    END IF;
+
+    IF min_deg < 0 THEN
+        RAISE EXCEPTION 'min_deg cannot be negative';
+    END IF;
+
+    IF max_deg <= min_deg THEN
+        RAISE EXCEPTION 'max_degree must be greater than min_degree';
+    END IF;
+
+    INSERT INTO "course" ("course_name", "min_degree", "max_degree")
+    VALUES (clean_name, min_deg, max_deg);
+
+    RAISE NOTICE 'Course "%" added successfully (min=%, max=%)', clean_name, min_deg, max_deg;
 
 EXCEPTION
     WHEN unique_violation THEN
-        RAISE EXCEPTION 'Course "%" already exists', name;
+        RAISE EXCEPTION 'Course "%" already exists', clean_name;
 
     WHEN check_violation THEN
         RAISE EXCEPTION 'Invalid course data (check name or degrees)';
@@ -24,23 +39,49 @@ $$;
 CREATE PROCEDURE "update_course" (id INT, name TEXT, min_deg INT, max_deg INT)
 LANGUAGE "plpgsql"
 AS $$
+DECLARE
+    clean_name TEXT := TRIM(name);
 BEGIN
+
+    IF name IS NULL OR clean_name = '' THEN
+        RAISE EXCEPTION 'Course name cannot be empty';
+    END IF;
+
+    IF min_deg < 0 THEN
+        RAISE EXCEPTION 'min_degree cannot be negative';
+    END IF;
+
+    IF max_deg <= min_deg THEN
+        RAISE EXCEPTION 'max_degree must be greater than min_degree';
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM "course"
+        WHERE "course_id" = id
+    ) THEN
+        RAISE EXCEPTION 'Course with ID % does not exist', id;
+    END IF;
+
+    IF EXISTS (
+        SELECT 1 FROM "course"
+        WHERE LOWER("course_name") = LOWER(clean_name)
+        AND "course_id" <> id
+    ) THEN
+        RAISE EXCEPTION 'Course "%" already exists', clean_name;
+    END IF;
+
     UPDATE "course"
     SET 
-        "course_name" = LOWER(TRIM(name)),
+        "course_name" = clean_name,
         "min_degree" = min_deg,
         "max_degree" = max_deg
     WHERE "course_id" = id;
 
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'Course with ID % does not exist', id;
-    END IF;
-
-    RAISE NOTICE 'Course "%" updated successfully (id=%)', name, id;
+    RAISE NOTICE 'Course "%" updated successfully (id=%)', clean_name, id;
 
 EXCEPTION
     WHEN unique_violation THEN
-        RAISE EXCEPTION 'Course "%" already exists', name;
+        RAISE EXCEPTION 'Course "%" already exists', clean_name;
     WHEN check_violation THEN
         RAISE EXCEPTION 'Invalid course data (check name or degrees)';
     WHEN OTHERS THEN
@@ -107,6 +148,19 @@ RETURNS TABLE (
 LANGUAGE "plpgsql"
 AS $$
 BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM "track" WHERE "track_id" = id
+    ) THEN
+        RAISE EXCEPTION 'Track with ID % does not exists', id;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM "track_course"
+        WHERE "track_id" = id
+    ) THEN
+        RAISE EXCEPTION 'No courses found for this track';
+    END IF;
+
     RETURN QUERY
     SELECT
         "course"."course_id",
