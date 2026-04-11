eff99df04b8cd9625e7e36e3560fb4a68b0b7fc5
elftiany <mk5920201@gmail.com>
2026-04-12 01:29:08 +0200

    Test Instructor (Updated - Update id exception not found) (Delete)

--- test_procedures/Instructor_test.sql
+++ test_procedures/Instructor_test.sql
@@ -5,3 +5,10 @@ CALL add_instructor('', 'test2@iti.com', 1);                --Exception: empty n
 CALL add_instructor('Test2', 'test@iti.com', 1);            -- Exteption: Dublicate Name
 
 
+CALL update_instructor(1, 'Updated Name', 'updated@iti.com', 1);           --Updated
+
+CALL update_instructor(999, 'Test', 't@t.com', 1);                     --Wrong id
+
+CALL delete_instructor(1);                                      --Deleted
+
+
